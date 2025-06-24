b603011c2077e85cf7629f4751b0ba8f1840283d
JANG-90 <wahall90@gmail.com>
2025-06-24 21:34:34 +0900

    6월 24일 타키로그 시작 장성우

---
+++ src/main/java/com/takilog/takibi/TakibiApplication.java
@@ -0,0 +1,13 @@
+package com.takilog.takibi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakibiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakibiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takilog/takibi/TakibiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takilog.takibi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakibiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
